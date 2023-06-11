668ef0b40960d6284a8ca63bb422214d0f4f0295
itaytui <itayta@post.bgu.ac.il>
2023-06-12 02:34:19 +0300

    finished without testing

--- src/AbstractSkipList.java
+++ src/AbstractSkipList.java
@@ -5,16 +5,20 @@ import java.util.List;
 abstract public class AbstractSkipList {
     final protected Node head;
     final protected Node tail;
+    protected int defaultLength;
+
 
     public AbstractSkipList() {
         head = new Node(Integer.MIN_VALUE);
         tail = new Node(Integer.MAX_VALUE);
         increaseHeight();
+        this.defaultLength=0;
     }
 
     public void increaseHeight() {
         head.addLevel(tail, null);
         tail.addLevel(null, head);
+        head.setLength(head.height,defaultLength);
     }
 
     abstract Node find(int key);
@@ -28,6 +32,10 @@ abstract public class AbstractSkipList {
     }
 
     public Node insert(int key) {
+        //
+        int sum=0;
+        int nextLevel=0;
+        //
         int nodeHeight = generateHeight();
 
         while (nodeHeight > head.height()) {
@@ -43,25 +51,56 @@ abstract public class AbstractSkipList {
 
         for (int level = 0; level <= nodeHeight && prevNode != null; ++level) {
             Node nextNode = prevNode.getNext(level);
-
             newNode.addLevel(nextNode, prevNode);
             prevNode.setNext(level, newNode);
             nextNode.setPrev(level, newNode);
-
+            //
+            newNode.setLength(level,prevNode.getLength(level)+1-sum);
+            prevNode.setLength(level,sum);
+            //
             while (prevNode != null && prevNode.height() == level) {
+                //
+                sum+=prevNode.getLength(level);
+                //
                 prevNode = prevNode.getPrev(level);
+
             }
         }
-
+        //
+        while(nextLevel<=head.height()) {
+            nextLevel++;
+            if (prevNode.height() >= nextLevel) {
+                prevNode.setLength(nextLevel,prevNode.getLength(nextLevel)+1);
+            } else {
+                while (prevNode != null && prevNode.height() < nextLevel) {
+                    prevNode = prevNode.getPrev(nextLevel - 1);
+                }
+                if (prevNode != null)
+                    prevNode.setLength(nextLevel, prevNode.getLength(nextLevel) + 1);
+            }
+        }
+        this.defaultLength++;
+        //
         return newNode;
     }
 
     public boolean delete(Node node) {
+        //
+        if (find(node.key).key != node.key || find(node.key)== head ) {
+            return false;
+        }
+        //
         for (int level = 0; level <= node.height(); ++level) {
             Node prev = node.getPrev(level);
             Node next = node.getNext(level);
             prev.setNext(level, next);
             next.setPrev(level, prev);
+            //
+            prev.setLength(level,prev.getLength(level)+node.getLength(level));
+            while (prev != null && prev.height() == level) {
+                prev = prev.getPrev(level);
+            }
+            //
         }
 
         return true;
@@ -116,17 +155,19 @@ abstract public class AbstractSkipList {
         return str.toString();
     }
 
-    public static class Node {
+    public static class  Node {
         final private List<Node> next;
         final private List<Node> prev;
         private int height;
         final private int key;
+        final private List<Integer> length;
 
         public Node(int key) {
             next = new ArrayList<>();
             prev = new ArrayList<>();
             this.height = -1;
             this.key = key;
+            this.length = new ArrayList<>();
         }
 
         public Node getPrev(int level) {
@@ -169,5 +210,27 @@ abstract public class AbstractSkipList {
 
         public int height() { return height; }
         public int key() { return key; }
+
+
+        public void setLength(int level, int length) {
+            if (level > height) {
+                throw new IllegalStateException("Fetching height higher than current node height");
+            }
+            while (level >= this.length.size()) {
+                this.length.add(0);
+            }
+            this.length.set(level, length);
+        }
+        public Integer getLength(int level) {
+            if (level > height) {
+                throw new IllegalStateException("Fetching height higher than current node height");
+            }
+            if (level > this.length.size()) {
+                return 0;
+            }
+            return this.length.get(level);
+        }
+
+
     }
 }
